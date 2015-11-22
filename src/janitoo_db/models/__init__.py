3e7f77e91797fb69341dca1d1408ec690968b935
bibi21000 <bibi21000@gmail.com>
2015-11-22 22:24:43 +0100

    Update db model

--- src/janitoo_db/models/__init__.py
+++ src/janitoo_db/models/__init__.py
@@ -34,8 +34,9 @@ import glob
 modules = [ basename(f)[:-3] for f in glob.glob(dirname(__file__)+"/*.py") if isfile(f) and not basename(f).startswith('_')]
 logger.info("Load core models %s", modules)
 for module in modules:
+    __import__(module, locals(), globals())
     #~ mod = __import__(module, locals(), globals())
-    __import__(module, locals(), globals()).extend(sys.modules[__name__])
+    #~ mod.extend(sys.modules[__name__])
     #~ del mod
 
 import pkg_resources
