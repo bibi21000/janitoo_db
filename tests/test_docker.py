d5ff5fe7467255623486938d3bbe2d8bdfde19af
bibi21000 <bibi21000@gmail.com>
2016-04-09 00:12:21 +0200

    Add test for docker

---
+++ tests/test_docker.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+"""Unittests for Janitoo-common.
+"""
+__license__ = """
+    This file is part of Janitoo.
+
+    Janitoo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Janitoo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Janitoo. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+__copyright__ = "Copyright © 2013-2014-2015-2016 Sébastien GALLET aka bibi21000"
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import sys, os
+import time
+import unittest
+import logging
+import threading
+import mock
+import logging
+
+from janitoo_nosetests import JNTTBase
+from janitoo_nosetests.server import JNTTDBDockerServerCommon, JNTTDBDockerServer
+
+from janitoo.runner import Runner, jnt_parse_args
+from janitoo.server import JNTServer
+from janitoo.utils import HADD_SEP, HADD
+
+class TestDBSerser(JNTTDBDockerServer, JNTTDBDockerServerCommon):
+    """Test the server
+    """
+    path = '/tmp/janitoo_test'
+    broker_user = 'toto'
+    broker_password = 'toto'
+    server_class = JNTDBServer
+    server_conf = "tests/data/janitoo_db_server.conf"
+    hadds = [HADD%(2218,0)]
